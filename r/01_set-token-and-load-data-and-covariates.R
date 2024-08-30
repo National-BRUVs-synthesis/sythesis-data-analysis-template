6faa9f7a2e741b48d1cbba4a45d5b7d0dd85070c
BrookeGibbons <brooke.gibbons@uwa.edu.au>
2024-08-30 11:32:09 +0800

    add script for template

---
+++ r/01_set-token-and-load-data-and-covariates.R
@@ -0,0 +1,49 @@
+#####################################################################
+# Project: API query for Australian BRUV synthesis
+# Data:    2024 BRUV Syntheses metadata, count, length and covariates
+# Task:    Use GlobalArchive API to query data and save as an RDS
+# Author:  Brooke Gibbons and Claude Spencer
+# Date:    Feb 2024
+#####################################################################
+
+# Install CheckEM package ----
+options(timeout = 9999999) # the package is large, so need to extend the timeout to enable the download.
+remotes::install_github("GlobalArchiveManual/CheckEM") # If there has been any updates to the package then CheckEM will install, if not then this line won't do anything
+
+# Load libraries needed -----
+library(CheckEM)
+library(httr)
+library(tidyverse)
+library(RJSONIO)
+library(devtools)
+
+# Set your API token to access GlobalArchive data shared with you ----
+# It is extremely important that you keep your API token out of your scripts, and github repository!
+# This function will ask you to put your API token in the console
+# It will then create a folder in your project folder called "secrets" and saves your API token to use in the functions later
+# The function adds the token into the .gitignore file so it will never be put in version control with Git
+CheckEM::ga_api_set_token()
+
+# Load the saved token
+token <- readRDS("secrets/api_token.RDS")
+
+# Load the covariate data set ----
+# We will update the covariate data set in the private repository
+# The function will warn you if you do not have access to the data
+covariates <- CheckEM::load_rds_from_github(url = "https://raw.githubusercontent.com/GlobalArchiveManual/australia-synthesis-2024/main/data/tidy/australian-synthesis_covariates.RDS")
+
+# Load the metadata, count and length ----
+# This way does not include the zeros where a species isn't present - it returns a much smaller dataframe
+CheckEM::ga_api_load_synthesis(synthesis_id = "19",
+                               token = token,
+                               dir = "data/raw/",
+                               include_zeros = FALSE)
+## This way DOES include the zeros where a species isn't present - it returns a much, much, larger dataframe
+# CheckEM::ga_api_load_synthesis(synthesis_id = "19", 
+#                                token = token, 
+#                                dir = "data/raw/", 
+#                                include_zeros = TRUE)
+
+# Example to filter data to species of interest ----
+count_filtered <- count %>%
+  dplyr::filter()
